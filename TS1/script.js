e339102ae4a3d32a771bcde03dcb18aa3f6c1144
ndnmndrgn <ndnmndrgn04@gmail.com>
2025-02-08 11:59:34 +0800

    02/08/2025 - commit with js

--- TS1/script.js
+++ TS1/script.js
@@ -1,10 +1,10 @@
-const resume = document.getElementById('resume');
+const resume = document.querySelector('.container');
 const colorChangeBtn = document.getElementById('colorChangeBtn');
 
 const colors = ['#89CFF0', '#0000FF', '#0096FF', '#6495ED', '#191970']; // Add more colors if needed
 let currentColorIndex = 0;
 
 colorChangeBtn.addEventListener('click', () => {
-resume.style.backgroundColor = colors[currentColorIndex];
-currentColorIndex = (currentColorIndex + 1) % colors.length;
+    resume.style.backgroundColor = colors[currentColorIndex];
+    currentColorIndex = (currentColorIndex + 1) % colors.length;
 });
